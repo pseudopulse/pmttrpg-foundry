0173fa7f58cbc5714b84409e43ccc1f8d4ee8a7a
psuedopulse <psuedopulse@protonmail.com>
2026-05-19 17:59:24 -0500

    w

--- module/core/combat/bars.mjs
+++ module/core/combat/bars.mjs
@@ -162,7 +162,7 @@ async function drawStatus(bar, token, scaleFactor) {
     let barPosY = 150;
     //
 
-    let maxIcons = Math.floor(2 * scaleFactor * (canvas.grid.size / 256));
+    let maxIcons = Math.floor(2 * scaleFactor * (256 / canvas.grid.size));
 
     let totalWidth = offset * Math.min(effects.length, maxIcons);
 
